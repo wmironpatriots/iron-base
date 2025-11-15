e21c36d8e04beb634ebfcba553ead9fa2aeea90c
Dasun Abeykoon <Dasun20202020@hotmail.com>
2025-11-15 18:21:02 -0500

    feat: replace generated robot program with CommandBased implementation
    Signed-off-by: Dasun Abeykoon <Dasun20202020@hotmail.com>

--- src/main/java/org/frc6423/robot/Robot.java
+++ src/main/java/org/frc6423/robot/Robot.java
@@ -6,69 +6,21 @@
 
 package org.frc6423.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import org.frc6423.lib.driver.CommandRobot;
 
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+public class Robot extends CommandRobot {
 
-  private final RobotContainer m_robotContainer;
+  public Robot() {}
 
-  public Robot() {
-    m_robotContainer = new RobotContainer();
+  @Override
+  protected Command getAutonCommand() {
+    return Commands.none();
   }
 
   @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
+  public void close() {
+    super.close();
   }
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void teleopExit() {}
-
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void testExit() {}
 }
